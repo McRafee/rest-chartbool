319f5f90ce9708b6d8641d4e59916eed2d6e14a9
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-04-08 11:09:24 +0200

    bugfix

--- js/main.js
+++ js/main.js
@@ -8,88 +8,71 @@ $(document).ready(function() {
     var dataFromApi;
     $.ajax(apiSettings).done(function(response) {
         dataFromApi = response;
-        console.log(dataFromApi); // debug
+        // console.log(dataFromApi); // debug
 
-        var objToFill = {};
-        var objToFill2 = {};
+        var objLineChart = {};
+        var objPieChart = {};
         var arrayMonths = moment.months();
         for (var i = 0; i < dataFromApi.length; i++) {
             var singleObj = dataFromApi[i];
             var date = singleObj.date;
             var month = moment(date, "DD/MM/YYYY").get("M");
             // console.log(month); //debug
-            if (objToFill[month] === undefined) { // in case the key doesn't exist
-                objToFill[month] = 0; //  then we create it and assign it the value 0
+            if (objLineChart[month] === undefined) { // in case the key doesn't exist
+                objLineChart[month] = 0; //  then we create it and assign it the value 0
             }
-            // console.log(objToFill); //debug
-            objToFill[month] += singleObj.amount; // (now the key exists) and to its value we add that of the amount of the ith single object that we are cycling
+            // console.log(objLineChart); //debug
+            objLineChart[month] += singleObj.amount; // (now the key exists) and to its value we add that of the amount of the ith single object that we are cycling
 
             var salesMan = singleObj.salesman;
-            if (objToFill2[salesMan] === undefined) { // in case the key doesn't exist
-                objToFill2[salesMan] = 0; //  then we create it and assign it the value 0
+            if (objPieChart[salesMan] === undefined) { // in case the key doesn't exist
+                objPieChart[salesMan] = 0; //  then we create it and assign it the value 0
             }
-            objToFill2[salesMan] += singleObj.amount; // (now the key exists) and to its value we add that of the amount of the ith single object that we are cycling
+            objPieChart[salesMan] += singleObj.amount; // (now the key exists) and to its value we add that of the amount of the ith single object that we are cycling
 
         }
-        // console.log(objToFill); //debug
 
         var labelsMonths = [];
         var dateAmount = [];
+        for (var key in objLineChart) { // cycle in the Intermediate object to take the keys and transform them into 'labels' and the values (of that key) to transform them into 'data'
+            labelsMonths.push(key);
+            dateAmount.push(objLineChart[key]);
+        }
 
         var labelsSalesMan = [];
         var salesManAmount = [];
-
-        for (var key in objToFill) { // cycle in the Intermediate object to take the keys and transform them into 'labels' and the values (of that key) to transform them into 'data'
-            // console.log(key);
-            labelsMonths.push(key);
-            dateAmount.push(objToFill[key]);
-        }
-
-        for (var key in objToFill2) { // cycle in the Intermediate object to take the keys and transform them into 'labels' and the values (of that key) to transform them into 'data'
-            // console.log(key);
+        for (var key in objPieChart) { // cycle in the Intermediate object to take the keys and transform them into 'labels' and the values (of that key) to transform them into 'data'
             labelsSalesMan.push(key);
-            salesManAmount.push((objToFill2[key]));
+            salesManAmount.push((objPieChart[key]));
         }
 
         var salesTotal = arraySum(salesManAmount);
-        // console.log(salesTotal);
 
         for (var i = 0; i < salesManAmount.length; i++) {
-            salesManAmount[i] = Math.round(((salesManAmount[i]/salesTotal)*100), -1);
-            // console.log(salesManAmount[i]);
+            // salesManAmount[i] = Math.round(((salesManAmount[i]/salesTotal)*100), -1);
+            salesManAmount[i] = ((salesManAmount[i]/salesTotal)*100).toFixed(2);
         }
-
-
-
-        console.log(labelsMonths);
-        console.log(dateAmount);
-
-        console.log(labelsSalesMan);
-        console.log(salesManAmount);
 
         var ctx = $('#sales-line-chart');
         var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'line',
-
-            // The data for our dataset
-            data: {
+            type: 'line', // The type of chart we want to create
+            data: { // The data for our dataset
                 labels: arrayMonths,
                 datasets: [{
                     label: 'Fatturato mensile',
-                    backgroundColor: 'rgba(0, 68, 131, 0.5)',
+                    pointRadius: "0",
+                    pointStyle: "circle",
+                    backgroundColor: 'rgba(0, 0, 0, 0.0)',
                     borderColor: 'rgb(0, 68, 131)',
+                    lineTension: '0',
                     data: dateAmount
                 }]
             },
-
-            // Configuration options go here
-            options: {}
+            options: {} // Configuration options go here
         });
 
         var ctx2 = $('#sales-pie-chart');
         var chart2 = new Chart(ctx2, {
-
         type: 'pie',
         data: {
             datasets: [{
@@ -99,11 +82,11 @@ $(document).ready(function() {
 
             labels: labelsSalesMan
         }
+        });
+
     });
 
-
-    });
-
+    // *** FUNCTIONS ***
     function arraySum(array){
         return array.reduce(function(a,b){
             return a + b
